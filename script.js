b0df5f2d2e224f4d6c53fe2f43ed2c3dd0389582
Tijana Pejic <tijana.trivic1993@gmail.com>
2022-12-11 20:12:22 +0100

    switch theme bug fix

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function toggleDarkLightMode(isDark) {
     textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
     isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    isDark ? imageMode('dark') : imageMode('light');
+    isDark === 'dark' ? imageMode('dark') : imageMode('light');
 }
 
 // Switch Theme Dynamically
